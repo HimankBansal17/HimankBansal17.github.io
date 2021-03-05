e9a8ad6adc3a9546b14647d3d6616bd88cf73ea5
Himank Bansal <himank13231726@gmail.com>
2021-03-06 01:14:10 +1000

    My Commit 2
    Testing website

---
+++ ClientApp/build/precache-manifest.f80aaa904567d217aa5e3df49a15d4fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41b4d3e5bb69ed08a6a683941d751ad",
+    "url": "/mycommunitylandmark/index.html"
+  },
+  {
+    "revision": "9c96c931f5cc1abb466a",
+    "url": "/mycommunitylandmark/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "6c3a32a8ecc4240b8b92",
+    "url": "/mycommunitylandmark/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "9c96c931f5cc1abb466a",
+    "url": "/mycommunitylandmark/static/js/2.c56a1861.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/mycommunitylandmark/static/js/2.c56a1861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c3a32a8ecc4240b8b92",
+    "url": "/mycommunitylandmark/static/js/main.6d84ea26.chunk.js"
+  },
+  {
+    "revision": "6f61bcafbef30b2a6e6b",
+    "url": "/mycommunitylandmark/static/js/runtime-main.2a08c48d.js"
+  }
+]);
